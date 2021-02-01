719dcef377d540d1b11701a26929ed03a13be5a3
Daniil Manin <manindaniil@protonmail.com>
2021-02-01 13:53:17 +0700

    update strings for cryptocurrencies

--- marketbar/code/extensions/Double+Extensions.swift
+++ marketbar/code/extensions/Double+Extensions.swift
@@ -14,6 +14,19 @@ extension Double {
 	}
 	
 	var priceString: String {
-		round2 == 0.0 ? "-" : "\(round2)"
+		round2 == 0.0 ? toString() : "\(round2)"
+	}
+	
+	// MARK: - Private
+
+	func toString(decimal: Int = 9) -> String {
+		let value = decimal < 0 ? 0 : decimal
+		var string = String(format: "%.\(value)f", self)
+		
+		while string.last == "0" || string.last == "." {
+			if string.last == "." { string = String(string.dropLast()); break}
+			string = String(string.dropLast())
+		}
+		return string
 	}
 }
